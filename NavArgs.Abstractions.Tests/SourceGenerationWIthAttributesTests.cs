37d1651da974a1f8a5fff541c5122d889e0072dd
Andres Hoyos <andres_hoyos@epam.com>
2025-05-30 13:35:08 -0500

    Add unit tests for source generator and enhance project structure
    Introduced `NavArgs.Abstractions.Tests` with initial test cases for source generation. Improved namespace handling for generated files and updated project files to include necessary dependencies. Added CI workflow for automated builds and releases.

---
+++ NavArgs.Abstractions.Tests/SourceGenerationWIthAttributesTests.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.RegularExpressions;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using NavArgs.Generator;
+
+namespace NavArgs.Abstractions.Tests;
+
+public partial class SourceGenerationWIthAttributesTests
+{
+    [Fact]
+    public void GenerateWithoutRouteParameter()
+    {
+        // create an instance of the source generator
+        var generator = new NavDestinationGenerator();
+
+        var driver = CSharpGeneratorDriver.Create(generator);
+        var compilation = CSharpCompilation.Create(nameof(SourceGenerationWIthAttributesTests),
+            new[]
+            {
+                CSharpSyntaxTree.ParseText(Constants.AccountDetailsWithoutRouteClass)
+            },
+            new[]
+            {
+                MetadataReference.CreateFromFile(typeof(object).Assembly.Location)
+            });
+
+        var runResult = driver.RunGenerators(compilation).GetRunResult();
+
+        var generatedFileSyntax = runResult.GeneratedTrees.Single(t => t.FilePath.EndsWith("AccountDetails.g.cs"));
+
+        var generatedFileText = generatedFileSyntax.GetText().ToString();
+        generatedFileText = SpaceRegex().Replace(generatedFileText, " ");
+        
+        Assert.Equal(SpaceRegex().Replace(Constants.ExpectedAccountDetailsWithoutRouteClass, " "), generatedFileText,
+            ignoreLineEndingDifferences: true, ignoreWhiteSpaceDifferences: true);
+    }
+
+    [GeneratedRegex(@"(\r\n|\r|\n)*")]
+    private static partial Regex SpaceRegex();
+}
